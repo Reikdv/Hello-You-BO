ff5410f1415bbf71a7dd97f1db22d4b620a3818e
Reikdv <37676@ma-web.nl>
2023-11-02 21:50:07 +0100

    beroepsopdracht

--- beroepsopdracht.js
+++ beroepsopdracht.js
@@ -1,106 +1,236 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-const story = [
-  "Daniel heeft eindelijk de financiering en de uitrusting bij elkaar om zijn zoektocht te beginnen. Moet hij zijn reis alleen maken of iemand meenemen voor hulp en gezelschap?",
-  "Alleen gaan.\nIemand meenemen.",
-  "Daniel heeft besloten om zijn beste vriend Mark mee te nemen op zijn reis. Ze staan aan het begin van de jungle. Moet Daniel de gemakkelijke, bekende route nemen of de avontuurlijke, onbekende route?",
-  "De gemakkelijke.\nDe avontuurlijke.",
-  "Tijdens hun reis merken Daniel en Mark dat ze een rivier moeten oversteken. Moeten ze een stevige brug proberen te vinden of de rivier met een zelfgemaakt vlot oversteken?",
-  "Een brug vinden.\nEen vlot maken.",
-  "Terwijl ze diep in de jungle zijn, horen ze vreemde geluiden van dieren in de verte. Moeten ze het geluid volgen om te ontdekken wat het is of zich stilhouden en hun koers vervolgen?",
-  "Het geluid volgen.\nHun koers vervolgen.",
-  "De geluiden blijken afkomstig van een zeldzame diersoort te zijn. Moeten ze proberen het dier te vangen voor onderzoek of het met rust laten?",
-  "Het dier vangen voor onderzoek.\nHet dier met rust laten.",
-  "Na enkele weken in de jungle te hebben doorgebracht, ontmoeten ze een lokale stam. Moeten ze de stam nader benaderen en proberen met hen te communiceren of hun afstand bewaren?",
-  "Benaderen en proberen te communiceren.\nAfstand bewaren.",
-  "De stam accepteert Daniel en Mark als gasten. Ze worden uitgenodigd voor een ceremonie. Moeten ze deelnemen aan de ceremonie of beleefd weigeren?",
-  "Deelnemen aan de ceremonie.\nBeleefd weigeren.",
-  "Tijdens de ceremonie krijgen Daniel en Mark een drankje aangeboden dat ze nog nooit eerder hebben gezien. Moeten ze het drinken of weigeren?",
-  "Het drankje drinken.\nHet drankje weigeren.",
-  "Het drankje blijkt veilig en verkwikkend te zijn. Daniel en Mark voelen zich op hun gemak in de stam. Moeten ze langer bij de stam blijven om meer te leren over hun cultuur of doorgaan met hun zoektocht?",
-  "Langer bij de stam blijven.\nDoorgaan met de zoektocht.",
-  "Na enkele maanden bij de stam te hebben doorgebracht, hoort Daniel een gerucht dat zijn biologische familie in de buurt zou kunnen zijn. Moeten ze het gerucht onderzoeken of blijven ze bij de stam?",
-  "Het gerucht onderzoeken.\nBij de stam blijven.",
-  "Het gerucht blijkt waar te zijn. Daniel vindt aanwijzingen die hem dichter bij zijn familie brengen. Moet hij zijn zoektocht voortzetten, ondanks het verdriet van het afscheid van de stam, of blijven en zijn nieuwe familie koesteren?",
-  "De zoektocht voortzetten.\nBij de stam blijven.",
-  "Terwijl Daniel zijn zoektocht voortzet, komt hij gevaarlijke dieren en natuurlijke obstakels tegen. Moet hij deze uitdagingen alleen aangaan of hulp vragen aan de lokale stamleden die hij heeft ontmoet?",
-  "Alleen de uitdagingen aangaan.\nHulp vragen aan de lokale stamleden.",
-  "Na een lange zoektocht komt Daniel eindelijk oog in oog te staan met zijn biologische familie. Moet hij zichzelf meteen bekendmaken of eerst observeren en afwachten?",
-  "Zichzelf meteen bekendmaken.\nObserveren en afwachten.",
-  "Als Daniel zichzelf bekendmaakt, blijken zijn biologische familieleden overweldigd en gelukkig te zijn. Ze willen dat hij blijft. Moet hij bij hen blijven of teruggaan naar de stam?",
-  "Bij zijn biologische familie blijven.\nTeruggaan naar de stam.",
-  "Als Daniel terugkeert naar de stam, verwelkomen ze hem met open armen en zijn blij dat hij zijn familie heeft gevonden. Moet hij bij de stam blijven of met zijn biologische familie zijn?",
-  "Bij de stam blijven.\nMet zijn biologische familie zijn.",
-  "Als Daniel bij zijn biologische familie blijft, leren ze hem hun manier van leven en tradities. Moet hij zijn werk als bioloog opgeven en zich volledig aanpassen aan hun cultuur of proberen een balans te vinden tussen beide?",
-  "Zijn werk als bioloog opgeven en zich volledig aanpassen aan hun cultuur.\nEen balans vinden tussen zijn werk als bioloog en hun cultuur.",
-  "Als Daniel een balans probeert te vinden, komt hij in conflict tussen zijn twee werelden. Moet hij zijn biologische familie vertellen over zijn werk als bioloog of het geheim houden om hun relatie te beschermen?",
-  "Zijn biologische familie vertellen over zijn werk.\nHet geheim houden.",
-  "Als Daniel besluit om zijn werk te onthullen, ontstaat er begrip tussen hem en zijn biologische familie. Ze respecteren zijn keuzes. Moet hij nu proberen om zijn werk te combineren met het helpen van zijn familie of zijn werk opgeven om hen te ondersteunen",
-  "Zijn werk proberen te combineren met het helpen van zijn familie.\nZijn werk opgeven om hen te ondersteunen.",
-  "Nu Daniel begrijpt dat hij zijn werk kan combineren met het helpen van zijn familie, moet hij de lokale overheid inlichten over hun behoeften en zijn onderzoeksexpertise aanbieden om hun levensomstandigheden te verbeteren. Moet hij de overheid benaderen en samenwerken met hen, of moet hij het familieleven buiten de overheid houden?",
-  "De overheid benaderen en samenwerken.\nHet familieleven buiten de overheid houden.",
-  "De situatie in de jungle wordt gecompliceerder door ongeoorloofde houtkap en milieuvernietiging. Moet Daniel de lokale overheid en internationale organisaties inlichten om actie te ondernemen of de zaak zelf proberen op te lossen?",
-  "De lokale overheid en internationale organisaties inlichten.\nDe zaak zelf proberen op te lossen.",
-  "Na vele jaren in de jungle, heeft Daniel een succesvolle balans gevonden tussen zijn werk als bioloog, het helpen van zijn familie en het behoud van de jungle. Moet hij de rest van zijn leven in de jungle doorbrengen of terugkeren naar de buitenwereld en zijn verhaal delen met de wereld?",
-  "De rest van zijn leven in de jungle doorbrengen.\nTerugkeren naar de buitenwereld en zijn verhaal delen met de wereld."
-];
-
-let currentQuestion = 0;
-let storyProgress = [];
+const readlineSync = require('readline-sync');
 
 function startGame() {
-  console.log(story[currentQuestion]);
-  rl.question('> ', (answer) => {
-    storyProgress.push(answer);
-    currentQuestion++;
-    if (currentQuestion < story.length) {
-      startGame();
-    } else {
-      displayEnding();
-      rl.close();
-    }
-  });
-}
+  console.log("Je wordt wakker en merkt dat je je in een vreemde omgeving bevindt. Alles om je heen is donker, en je hebt geen idee hoe je hier terecht bent gekomen.");
+  console.log("Je hoort geluiden in de verte, en het lijkt erop dat je niet alleen bent. Wat wil je doen?");
+  console.log("a. Probeer te bewegen in de richting van de geluiden.");
+  console.log("b. Blijf stil liggen en probeer te begrijpen waar je bent.");
 
-function displayEnding() {
-  console.log('Einde van het verhaal');
-  const ending = calculateEnding();
-  console.log(`Einde: ${ending}`);
-}
+  const answer = getUserInput();
 
-function calculateEnding() {
-    if (
-      storyProgress[0] === 'Alleen gaan' &&
-      storyProgress[1] === 'De avontuurlijke, onbekende route' &&
-      storyProgress[2] === 'Een vlot maken' &&
-      storyProgress[3] === 'Het geluid volgen' &&
-      storyProgress[4] === 'Het dier met rust laten' &&
-      storyProgress[5] === 'Afstand bewaren' &&
-      storyProgress[6] === 'Beleefd weigeren' &&
-      storyProgress[7] === 'Het drankje drinken' &&
-      storyProgress[8] === 'Langer bij de stam blijven' &&
-      storyProgress[9] === 'Het gerucht onderzoeken' &&
-      storyProgress[10] === 'De zoektocht voortzetten' &&
-      storyProgress[11] === 'Hulp vragen aan de lokale stamleden' &&
-      storyProgress[12] === 'Zichzelf meteen bekendmaken' &&
-      storyProgress[13] === 'Bij zijn biologische familie blijven' &&
-      storyProgress[14] === 'Met zijn biologische familie zijn' &&
-      storyProgress[15] === 'Een balans vinden tussen zijn werk als bioloog en hun cultuur' &&
-      storyProgress[16] === 'Zijn biologische familie vertellen over zijn werk' &&
-      storyProgress[17] === 'Zijn werk proberen te combineren met het helpen van zijn familie' &&
-      storyProgress[18] === 'De overheid benaderen en samenwerken' &&
-      storyProgress[19] === 'De lokale overheid en internationale organisaties inlichten' &&
-      storyProgress[20] === 'Terugkeren naar de buitenwereld en zijn verhaal delen met de wereld'
-    ) {
-      return 'Einde 1: Daniel leidt een succesvolle expeditie en deelt zijn ervaringen met de wereld.';
-    } else {
-      return 'Einde 2: Daniel blijft in de jungle en vindt geluk bij zijn nieuwe familie.';
-    }
+  if (answer === 'a') {
+    exploreEnvironment();
+  } else if (answer === 'b') {
+    tryToUnderstand();
+  } else {
+    invalidInput();
   }
+}
+
+function exploreEnvironment() {
+  console.log("Voorzichtig sta je op en probeert je een weg te banen door de duisternis. Na een tijdje kom je aan bij een verlichte ruimte.");
+  console.log("In deze kamer zie je vreemde machines en knipperende schermen. Het lijkt wel een laboratorium.");
+  console.log("Wat wil je doen?");
+  console.log("a. Onderzoek de machines en schermen.");
+  console.log("b. Verlaat de kamer en ga verder op verkenning.");
+
+  const answer = getUserInput();
+
+  if (answer === 'a') {
+    examineLab();
+  } else if (answer === 'b') {
+    continueExploring();
+  } else {
+    invalidInput();
+  }
+}
+
+function tryToUnderstand() {
+  console.log("Je probeert te luisteren naar de geluiden om je heen en merkt op dat het klinkt als een druk laboratorium.");
+  console.log("Misschien ben je in een experiment beland. Wat wil je doen?");
+  console.log("a. Wachten om te zien of er iemand komt.");
+  console.log("b. Ga op verkenning in de duisternis.");
+
+  const answer = getUserInput();
+
+  if (answer === 'a') {
+    waitForHelp();
+  } else if (answer === 'b') {
+    exploreEnvironment();
+  } else {
+    invalidInput();
+  }
+}
+
+function examineLab() {
+  console.log("Terwijl je de machines en schermen onderzoekt, hoor je voetstappen achter je. Je draait je om en ziet een wetenschapper in een witte jas.");
+  console.log("Hij lijkt verbaasd dat je hier bent en stelt zich voor als Dr. Alexander. Hij legt uit dat je deelneemt aan een experiment om tijdreizen te testen.");
+  console.log("Wat wil je doen?");
+  console.log("a. Geloof Dr. Alexander en stel vragen over het experiment.");
+  console.log("b. Probeer te ontsnappen uit het laboratorium.");
+
+  const answer = getUserInput();
+
+  if (answer === 'a') {
+    askAboutTimeTravelExperiment();
+  } else if (answer === 'b') {
+    escapeFromLab();
+  } else {
+    invalidInput();
+  }
+}
+
+function continueExploring() {
+  console.log("Je verlaat de verlichte kamer en gaat verder met je verkenningstocht. Terwijl je door de donkere gangen sluipt, kom je een mysterieuze deur tegen.");
+  console.log("Wat wil je doen?");
+  console.log("a. Probeer de deur te openen.");
+  console.log("b. Keer terug naar de verlichte kamer.");
+
+  const answer = getUserInput();
+
+  if (answer === 'a') {
+    openMysteriousDoor();
+  } else if (answer === 'b') {
+    exploreEnvironment();
+  } else {
+    invalidInput();
+  }
+}
+
+function waitForHelp() {
+  console.log("Je besluit te wachten in de hoop dat iemand je komt redden. Na enige tijd komt Dr. Alexander terug met medische apparatuur.");
+  console.log("Hij zegt dat het experiment fout is gegaan en dat ze je zullen helpen terwijl ze proberen de situatie te herstellen.");
+  console.log("Wat wil je doen?");
+  console.log("a. Vertrouw Dr. Alexander en laat je behandelen.");
+  console.log("b. Blijf argwanend en probeer te ontsnappen.");
+
+  const answer = getUserInput();
+
+  if (answer === 'a') {
+    trustDoctorAndReceiveTreatment();
+  } else if (answer === 'b') {
+    escapeFromLab();
+  } else {
+    invalidInput();
+  }
+}
+
+function askAboutTimeTravelExperiment() {
+  console.log("Je begint Dr. Alexander vragen te stellen over het tijdreisexperiment en probeert meer te weten te komen over hoe je hier bent beland.");
+  console.log("Hij legt uit dat het experiment niet helemaal is verlopen zoals gepland, en dat je nu vastzit in een andere tijdlijn.");
+  console.log("Wat wil je doen?");
+  console.log("a. Werk samen met Dr. Alexander om een oplossing te vinden.");
+  console.log("b. Probeer op eigen kracht een manier te vinden om naar huis terug te keren.");
+
+  const answer = getUserInput();
+
+  if (answer === 'a') {
+    cooperateWithDoctor();
+  } else if (answer === 'b') {
+    findYourOwnWay();
+  } else {
+    invalidInput();
+  }
+}
+
+function escapeFromLab() {
+  console.log("Je probeert te ontsnappen uit het laboratorium. Terwijl je door de gangen rent, kom je verschillende bewakers en camera's tegen.");
+  console.log("Het lijkt erop dat ze je niet willen laten gaan. Wat wil je doen?");
+  console.log("a. Ga door met je ontsnappingspoging en vecht tegen de bewakers.");
+  console.log("b. Keer terug naar Dr. Alexander en vraag om hulp bij je ontsnapping.");
+
+  const answer = getUserInput();
+
+  if (answer === 'a') {
+    fightGuards();
+  } else if (answer === 'b') {
+    seekHelpFromDoctor();
+  } else {
+    invalidInput();
+  }
+}
+
+function openMysteriousDoor() {
+  console.log("Je probeert de mysterieuze deur te openen en ontdekt een geheime kamer met allerlei technologische apparatuur.");
+  console.log("Dit zou weleens de sleutel kunnen zijn tot je ontsnapping. Wat wil je doen?");
+  console.log("a. Onderzoek de technologische apparatuur.");
+  console.log("b. Sluit de deur en keer terug naar de verlichte kamer.");
+
+  const answer = getUserInput();
+
+  if (answer === 'a') {
+    investigateTechnology();
+  } else if (answer === 'b') {
+    exploreEnvironment();
+  } else {
+    invalidInput();
+  }
+}
+
+function trustDoctorAndReceiveTreatment() {
+  console.log("Je besluit Dr. Alexander te vertrouwen en laat je behandelen. Hij vertelt je dat ze bezig zijn met het herstellen van het tijdreisexperiment.");
+  console.log("Na verloop van tijd word je teruggebracht naar je eigen tijdlijn en bevind je je weer in je vertrouwde omgeving.");
+  console.log("Gefeliciteerd, je hebt de tijdreisavontuur overleefd!");
+
+  playAgain();
+}
+
+function cooperateWithDoctor() {
+  console.log("Je besluit samen te werken met Dr. Alexander om een oplossing te vinden voor het tijdreisprobleem.");
+  console.log("Samen werken jullie aan het herstellen van het experiment en na verloop van tijd word je veilig teruggebracht naar je eigen tijdlijn.");
+  console.log("Gefeliciteerd, je hebt de tijdreisavontuur overleefd!");
+
+  playAgain();
+}
+
+function findYourOwnWay() {
+  console.log("Je gaat op zoek naar een manier om op eigen kracht terug te keren naar je eigen tijdlijn.");
+  console.log("Na veel avonturen en obstakels lukt het je eindelijk om terug te keren naar je eigen tijdlijn.");
+  console.log("Gefeliciteerd, je hebt de tijdreisavontuur overleefd!");
+
+  playAgain();
+}
+
+function fightGuards() {
+  console.log("Je probeert te vechten tegen de bewakers, maar ze overmeesteren je en brengen je terug naar de verlichte kamer.");
+  console.log("Je hebt gefaald in je ontsnappingspoging. Wat wil je nu doen?");
+  console.log("a. Probeer opnieuw te ontsnappen.");
+  console.log("b. Werk samen met Dr. Alexander om een oplossing te vinden.");
+
+  const answer = getUserInput();
+
+  if (answer === 'a') {
+    escapeFromLab();
+  } else if (answer === 'b') {
+    seekHelpFromDoctor();
+  } else {
+    invalidInput();
+  }
+}
+
+function seekHelpFromDoctor() {
+  console.log("Je zoekt hulp bij Dr. Alexander en vraagt om zijn hulp bij je ontsnapping.");
+  console.log("Hij stemt ermee in om je te helpen en na een moeilijke ontsnappingspoging slagen jullie erin om te ontsnappen uit het laboratorium.");
+  console.log("Gefeliciteerd, je hebt de tijdreisavontuur overleefd!");
+
+  playAgain();
+}
+
+function investigateTechnology() {
+  console.log("Terwijl je de technologische apparatuur onderzoekt, ontdek je dat dit de apparatuur is waarmee ze tijdreizen uitvoeren.");
+  console.log("Je besluit het te activeren en terug te keren naar je eigen tijdlijn.");
+  console.log("Gefeliciteerd, je hebt de tijdreisavontuur overleefd!");
+
+  playAgain();
+}
+
+function playAgain() {
+  console.log("Wil je het avontuur opnieuw spelen?");
+  console.log("a. Ja");
+  console.log("b. Nee");
+
+  const answer = getUserInput();
+
+  if (answer === 'a') {
+    startGame();
+  } else {
+    console.log("Bedankt voor het spelen van het avontuur. Tot de volgende keer!");
+  }
+}
+
+function getUserInput() {
+  let answer = readlineSync.question('Welke keuze maak je? (A / B): ').toLowerCase();
+  return answer;
+}
 
 startGame();
